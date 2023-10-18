51311fda8501995d7ce85b869504509562214ed9
brettphelps32 <brettphelps32@gmail.com>
2023-10-17 21:22:21 -0500

    trying to figure out js with login

---
+++ Client/Scripts/Login.js
@@ -0,0 +1,16 @@
+function login(){
+    var userEmail = document.getElementById(userEmail);
+    // const userPassword = document.getElementById(userPassword);
+
+    // if(userEmail == "root@gmail.com"){
+    //     console.log("Logging in")
+    // }
+    // else{
+    //     console.log("User Not found")
+    // }
+    alert(userEmail)
+}
+
+function hello(){
+    alert('hello');
+}
