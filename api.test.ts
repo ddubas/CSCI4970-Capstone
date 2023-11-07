9693b953ecd2bc4e143ab5998adee09eada03f09
joe <jwilliams128@unomaha.edu>
2023-11-06 19:11:10 -0600

    testing setup files2

--- api.test.ts
+++ api.test.ts
@@ -3,6 +3,31 @@ const app = require('./express-app.ts'); // Import your Express app instance
 const db = require('./test-database.ts'); // Import the test database setup
 
 // Your test code that uses 'app' and 'db'
+// Test the Express app
+describe('Express App', () => {
+  // Define a test case for a specific route
+  it('should return "Hello, World!" from the root route', async (done) => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, World!');
+    done();
+  });
+
+  // Add more test cases for other routes and scenarios
+});
+
+// Test the database
+describe('Database', () => {
+  // Define a test case to check if a table contains data
+  it('should have data in the "users" table', (done) => {
+    db.all('SELECT * FROM users', [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      expect(rows.length).toBeGreaterThan(0);
+      done();
+    });
+  });
 
 
 // Export the app for testing
